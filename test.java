a4018526e43419a1df498fee90637d17fbb0e0b3
SUNSHINEGX <793854807@qq.com>
2016-04-21 12:41:45 +0800

    '第四次修改'

--- test.java
+++ test.java
@@ -1,5 +1,8 @@
 class test{
    public static void main( String [ ]args){
+	        int a;
+			int b;
+			System.out.println(a+b);
             System.out.println("hello"); 
             System.out.println("hello"); 
 			System.out.prinln("world");
