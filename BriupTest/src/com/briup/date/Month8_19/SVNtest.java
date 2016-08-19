e90a734a13605a068adabbe282b2345c63b50c50
SUNSHINEGX <793854807@qq.com>
2016-08-19 15:59:24 +0800

    819_1600

--- BriupTest/src/com/briup/date/Month8_19/SVNtest.java
+++ BriupTest/src/com/briup/date/Month8_19/SVNtest.java
@@ -5,6 +5,7 @@ public class SVNtest {
 	public static void main(String[] args) {
 		System.out.println("helloworld");
 		System.out.println("NongFuSpring");
+		System.out.println("NongFuSpring");
 		
 	}
 
