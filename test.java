d383161345e4364135a8270b7e1f5671a4242d67
Allen <867892406@qq.com>
2020-08-05 23:19:17 +0800

    给a,b赋值1，2

--- test.java
+++ test.java
@@ -1,11 +1,11 @@
 class test{
    public static void main( String [ ]args){
-	        int a;
-			int b;
-			System.out.println(a+b);
-            System.out.println("hello"); 
-            System.out.println("hello"); 
-			System.out.prinln("world");
+		int a = 1;
+		int b = 2;
+		System.out.println(a+b);
+        System.out.println("hello"); 
+        System.out.println("hello"); 
+		System.out.prinln("world");
 
   }
 }
