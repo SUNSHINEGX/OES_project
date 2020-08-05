1bdcaebfa93486d82eb2f80f62849b621376ec04
Allen <867892406@qq.com>
2020-08-05 23:31:38 +0800

    给a,b赋值 3，4

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 class test{
    public static void main( String [ ]args){
-	        int a;
-			int b;
+	        int a = 3;
+			int b = 4;
 			System.out.println(a+b);
             System.out.println("hello"); 
             System.out.println("hello"); 
